7ef7dc2b7c5cac6b697dcd5f00b8e493bcc8e9c0
btai <btai@users.noreply.github.com>
2017-08-14 12:11:42 +0200

    commit everything expect the uploads directory that contains media files

---
+++ wp-content/themes/genesis-sample/functions.php
@@ -0,0 +1,225 @@
+<?php
+/**
+ * Genesis Sample.
+ *
+ * This file adds functions to the Genesis Sample Theme.
+ *
+ * @package Genesis Sample
+ * @author  StudioPress
+ * @license GPL-2.0+
+ * @link    http://www.studiopress.com/
+ */
+
+// Start the engine.
+include_once( get_template_directory() . '/lib/init.php' );
+
+// Setup Theme.
+include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
+
+// Set Localization (do not remove).
+add_action( 'after_setup_theme', 'genesis_sample_localization_setup' );
+function genesis_sample_localization_setup(){
+	load_child_theme_textdomain( 'genesis-sample', get_stylesheet_directory() . '/languages' );
+}
+
+// Add the helper functions.
+include_once( get_stylesheet_directory() . '/lib/helper-functions.php' );
+
+// Add Image upload and Color select to WordPress Theme Customizer.
+require_once( get_stylesheet_directory() . '/lib/customize.php' );
+
+// Include Customizer CSS.
+include_once( get_stylesheet_directory() . '/lib/output.php' );
+
+// Add WooCommerce support.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-setup.php' );
+
+// Add the required WooCommerce styles and Customizer CSS.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-output.php' );
+
+// Add the Genesis Connect WooCommerce notice.
+include_once( get_stylesheet_directory() . '/lib/woocommerce/woocommerce-notice.php' );
+
+// Child theme (do not remove).
+define( 'CHILD_THEME_NAME', 'Genesis Sample' );
+define( 'CHILD_THEME_URL', 'http://www.studiopress.com/' );
+define( 'CHILD_THEME_VERSION', '2.3.0' );
+
+// Enqueue Scripts and Styles.
+add_action( 'wp_enqueue_scripts', 'genesis_sample_enqueue_scripts_styles' );
+function genesis_sample_enqueue_scripts_styles() {
+
+	wp_enqueue_style( 'genesis-sample-fonts', '//fonts.googleapis.com/css?family=Source+Sans+Pro:400,600,700', array(), CHILD_THEME_VERSION );
+	wp_enqueue_style( 'dashicons' );
+
+	$suffix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
+	wp_enqueue_script( 'genesis-sample-responsive-menu', get_stylesheet_directory_uri() . "/js/responsive-menus{$suffix}.js", array( 'jquery' ), CHILD_THEME_VERSION, true );
+	wp_localize_script(
+		'genesis-sample-responsive-menu',
+		'genesis_responsive_menu',
+		genesis_sample_responsive_menu_settings()
+	);
+
+}
+
+// Define our responsive menu settings.
+function genesis_sample_responsive_menu_settings() {
+
+	$settings = array(
+		'mainMenu'          => __( 'Menu', 'genesis-sample' ),
+		'menuIconClass'     => 'dashicons-before dashicons-menu',
+		'subMenu'           => __( 'Submenu', 'genesis-sample' ),
+		'subMenuIconsClass' => 'dashicons-before dashicons-arrow-down-alt2',
+		'menuClasses'       => array(
+			'combine' => array(
+				'.nav-primary',
+				'.nav-header',
+			),
+			'others'  => array(),
+		),
+	);
+
+	return $settings;
+
+}
+
+// Add HTML5 markup structure.
+add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
+
+// Add Accessibility support.
+add_theme_support( 'genesis-accessibility', array( '404-page', 'drop-down-menu', 'headings', 'rems', 'search-form', 'skip-links' ) );
+
+// Add viewport meta tag for mobile browsers.
+add_theme_support( 'genesis-responsive-viewport' );
+
+// Add support for custom header.
+add_theme_support( 'custom-header', array(
+	'width'           => 600,
+	'height'          => 160,
+	'header-selector' => '.site-title a',
+	'header-text'     => false,
+	'flex-height'     => true,
+) );
+
+// Add support for custom background.
+add_theme_support( 'custom-background' );
+
+// Add support for after entry widget.
+add_theme_support( 'genesis-after-entry-widget-area' );
+
+// Add support for 3-column footer widgets.
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+// Add Image Sizes.
+add_image_size( 'featured-image', 720, 400, TRUE );
+
+// Rename primary and secondary navigation menus.
+add_theme_support( 'genesis-menus', array( 'primary' => __( 'After Header Menu', 'genesis-sample' ), 'secondary' => __( 'Footer Menu', 'genesis-sample' ) ) );
+
+// Reposition the secondary navigation menu.
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+add_action( 'genesis_footer', 'genesis_do_subnav', 5 );
+
+// Reduce the secondary navigation menu to one level depth.
+add_filter( 'wp_nav_menu_args', 'genesis_sample_secondary_menu_args' );
+function genesis_sample_secondary_menu_args( $args ) {
+
+	if ( 'secondary' != $args['theme_location'] ) {
+		return $args;
+	}
+
+	$args['depth'] = 1;
+
+	return $args;
+
+}
+
+// Modify size of the Gravatar in the author box.
+add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
+function genesis_sample_author_box_gravatar( $size ) {
+	return 90;
+}
+
+// Modify size of the Gravatar in the entry comments.
+add_filter( 'genesis_comment_list_args', 'genesis_sample_comments_gravatar' );
+function genesis_sample_comments_gravatar( $args ) {
+
+	$args['avatar_size'] = 60;
+
+	return $args;
+
+}
+
+ //* Enqueue Google Fonts
+add_action( 'wp_enqueue_scripts', 'genesis_sample_google_fonts' );
+function genesis_sample_google_fonts() {
+
+	wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i', array(), CHILD_THEME_VERSION );
+    }
+
+
+    add_filter( 'gform_confirmation_anchor', '__return_true' );
+
+    /**
+ * Display Next/Prev link on single posts
+ *
+ * @since 1.0.0
+ */
+function ja_prev_next_post_nav() {
+	if ( is_singular( 'post' ) ) {
+		echo '<div class="prev-next-navigation">';
+			previous_post_link( '<div class="next">%link</div>', 'Eldre <img src="http://imbera.nyhjemmeside.no/wp-content/uploads/2017/06/neste.png" alt="%title" title="%title" style=" width:21px; margin-top:10px;"/>' );
+			next_post_link( '<div class="previous">%link</div>', '<img src="http://imbera.nyhjemmeside.no/wp-content/uploads/2017/06/forrige.png" alt="%title" title="%title" style=" width:21px; margin-top:10px;" /> Nyere ' );
+
+            //previous_post_link( '<div class="previous">%link</div>', '<img src="http://imbera.nyhjemmeside.no/wp-content/uploads/2017/06/forrige.png" alt="%title" title="%title" style=" width:21px; margin-top:10px;"/> %title' );
+            //next_post_link( '<div class="next">%link</div>', '%title <img src="http://imbera.nyhjemmeside.no/wp-content/uploads/2017/06/neste.png" alt="%title" title="%title" style=" width:21px; margin-top:10px;" />' );
+
+		echo '</div>';
+	}
+}
+add_action( 'genesis_after_entry', 'ja_prev_next_post_nav' );
+
+
+/**
+ * Add Genesis Title Toggle to Events
+ *
+ * @link https://github.com/billerickson/Genesis-Title-Toggle/issues/4
+ * @author Bill Erickson
+ *
+ * @param array $post_types
+ * @return array
+ */
+function be_title_toggle_on_events( $post_types ) {
+  $post_types[] = 'post';
+  return $post_types;
+}
+add_filter( 'be_title_toggle_post_types', 'be_title_toggle_on_events' );
+
+/** Enabling Categories and Tags Taxonomy for pages at Add/Edit Screen
+ * 
+ * @since 1.0
+ * 
+ */ 
+add_action('init', 'gd_register_category_tags_taxonomy_for_page');
+function gd_register_category_tags_taxonomy_for_page() {
+	register_taxonomy_for_object_type('post_tag', 'page');
+	register_taxonomy_for_object_type('category', 'page'); 
+}
+
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
+
+function hikeitbaby_custom_header_setup() {
+    add_theme_support( 'custom-header', apply_filters( 'hikeitbaby_custom_header_args', array(
+        'default-image'          => '',
+        'default-text-color'     => '000000',
+        'flex-width'             => true,
+        'flex-height'            => true,
+       
+    ) ) );
+}
+add_action( 'after_setup_theme', 'hikeitbaby_custom_header_setup' );
