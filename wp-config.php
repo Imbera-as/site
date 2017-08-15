d5f92ad3463beb9fb36797f1831209debf01f324
btai <sudhirpandey@users.noreply.github.com>
2017-08-15 11:19:22 +0200

    env based siteurl and hostname
    pull in the siteurl and hostname value from environment variables

--- wp-config.php
+++ wp-config.php
@@ -25,9 +25,12 @@ $user=getenv('MYSQL_USER');
 $password=getenv('MYSQL_PASSWORD');
 $access_key_id=getenv('AWS_ACCESS_KEY_ID');
 $access_secret=getenv('AWS_ACCESS_SECRET_KEY');
+$siteurl=getenv('HOSTNAME')
+$hostname=getenv('HOSTNAME')
 
-
-
+define('WP_HOME',$hostname);
+define('WP_SITEURL',$siteurl);
+	
 define('DB_NAME', 'imbera2017');
 
 /** MySQL database username */
